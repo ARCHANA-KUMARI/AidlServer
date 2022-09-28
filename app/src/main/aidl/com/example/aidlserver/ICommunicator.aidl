283d485f81b96a2dbe96df6d54b8e96df24936bb
ARCHANA-KUMARI <ms.archanacusat2015@gmail.com>
2022-09-28 15:15:09 +0530

    Aidl Server service impl done

---
+++ app/src/main/aidl/com/example/aidlserver/ICommunicator.aidl
@@ -0,0 +1,8 @@
+// ICommunicator.aidl
+package com.example.aidlserver;
+
+// Declare any non-default types here with import statements
+
+interface ICommunicator {
+   int calculate(int num1 , int num2);
+}
---
+++ app/src/main/java/com/example/aidldemo/services/CalculatorService.java
@@ -0,0 +1,29 @@
+package com.example.aidldemo.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.example.aidlserver.ICommunicator;
+
+public class CalculatorService extends Service {
+    private static String TAG = "CalculatorService";
+    public CalculatorService() {
+    }
+    private  CalculatorStub calculatorStub = new CalculatorStub();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG, "onBind: intent" + intent);
+        return calculatorStub;
+    }
+    class CalculatorStub extends ICommunicator.Stub {
+
+        @Override
+        public int calculate(int num1, int num2) throws RemoteException {
+            return num1 + num2;
+        }
+    }
+}
