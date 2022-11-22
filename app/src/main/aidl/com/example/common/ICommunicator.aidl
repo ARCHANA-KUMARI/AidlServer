340034b281d296300c25e3490622c51aaa67d9d8
ARCHANA-KUMARI <ms.archanacusat2015@gmail.com>
2022-11-22 19:53:38 +0530

    Aidl file package name change and added one more method in aidl file

--- app/src/main/aidl/com/example/common/ICommunicator.aidl
+++ app/src/main/aidl/com/example/common/ICommunicator.aidl
@@ -1,8 +1,9 @@
 // ICommunicator.aidl
-package com.example.aidlserver;
+package com.example.common;
 
 // Declare any non-default types here with import statements
 
 interface ICommunicator {
    int calculate(int num1 , int num2);
+   String getName();
 }
--- app/src/main/java/com/example/aidldemo/services/CalculatorService.java
+++ app/src/main/java/com/example/aidldemo/services/CalculatorService.java
@@ -6,10 +6,10 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 
-import com.example.aidlserver.ICommunicator;
+import com.example.common.ICommunicator;
 
 public class CalculatorService extends Service {
-    private static String TAG = "CalculatorService";
+    private static String TAG = "ArchanaCalculatorService";
     public CalculatorService() {
     }
     private  CalculatorStub calculatorStub = new CalculatorStub();
@@ -19,11 +19,20 @@ public class CalculatorService extends Service {
         Log.i(TAG, "onBind: intent" + intent);
         return calculatorStub;
     }
+
     class CalculatorStub extends ICommunicator.Stub {
 
         @Override
         public int calculate(int num1, int num2) throws RemoteException {
             return num1 + num2;
         }
+
+        @Override
+        public String getName() throws RemoteException {
+           // return "Archana Kumari From Server";
+            return null;
+        }
+
     }
+
 }
